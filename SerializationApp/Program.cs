ee8549a733b702e9e0c868311b50cec5a51a8138
alice.kovacs <alice.kovacs@gmail.com>
2018-10-01 21:48:50 +0200

    Class is serializable, test is missing

--- SerializationApp/Program.cs
+++ SerializationApp/Program.cs
@@ -10,6 +10,13 @@ namespace SerializationApp
     {
         static void Main(string[] args)
         {
+
+            SerializePeople SerializePeople1 = new SerializePeople("John", new DateTime(2012, 10, 21), Genders.Male);
+            Console.WriteLine(SerializePeople1);
+
+            SerializePeople SerializePeople2 = new SerializePeople("Heidi", new DateTime(1998, 03, 06), Genders.Female);
+            Console.WriteLine(SerializePeople2);
+            Console.ReadKey();
         }
     }
 }
---
+++ SerializationApp/SerializePeople.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace SerializationApp
+{
+    public enum Genders { Male, Female }
+
+    [Serializable]
+    public class SerializePeople
+    {
+        protected string Name { get; set; }
+        protected DateTime BirthDate { get; set; }
+        protected Genders Gender { get; set; }
+        protected int Age { get; }
+
+        public SerializePeople(string name, DateTime birthDate, Genders gender)
+        {
+            Name = name;
+            BirthDate = birthDate;
+            Gender = gender;
+            Age = DateTime.Now.Year - BirthDate.Year;
+        }
+
+        public override string ToString()
+        {
+            return "SerializePeople: " + Name + ", " + BirthDate + ", " + Gender;
+        }
+    }
+}
